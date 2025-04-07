c49a3ea3282d1937304698acc00ceddf5b1aa47a
Wide97 <mwidesott@gmail.com>
2025-04-07 09:43:49 +0200

    Initial commit

---
+++ prenotazione-meeting/src/main/java/com/marco/prenotazione_meeting/PrenotazioneMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.marco.prenotazione_meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotazioneMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenotazioneMeetingApplication.class, args);
+	}
+
+}
---
+++ prenotazione-meeting/src/test/java/com/marco/prenotazione_meeting/PrenotazioneMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.prenotazione_meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotazioneMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
